1cb6be06edfbecd558c3eb76b1fb50c664ef7e02
David Corvoysier <david.corvoysier@orange.com>
2013-06-25 14:11:00 +0200

    Added filter registration method and moved existing skin filter into its own file

---
+++ src/filters/skin.js
@@ -0,0 +1,36 @@
+/**
+ * @author David Corvoysier / Copyright Orange 2013
+ */
+(function(global) {
+
+    // Register this filter
+    global.addFilter(
+        // Filter name
+        'skin',
+        // Vertex Shader (null uses default)
+        null,
+        // Fragment Shader (null uses default)        
+        '\
+        uniform sampler2D texture;\
+        varying vec2 texCoord;\
+        void main() {\
+            vec4 color = texture2D(texture, texCoord);\
+            float r = color.r;\
+            float g = color.g;\
+            float b = color.b;\
+            \
+            if ((r>45.0/255.0)&&(g>40.0/255.0)&&(b>20.0/255.0)\
+                &&(r>g)&&(r>b)\
+                &&(r-min(g,b)>15.0/255.0)\
+                &&(abs(r-g)>15.0/255.0)){\
+                gl_FragColor = color;\
+            } else {\
+                gl_FragColor = vec4(0.0,0.0,0.0,color.a);\
+            }\
+        }\
+        ',
+        // Uniforms callback
+        null
+    );
+    
+})(glimp);
