4bf9c43f4caa174dc2ceaa0341a788cb515bd3c7
David Corvoysier <david.corvoysier@orange.com>
2013-06-28 11:15:52 +0200

    glimp_filter: get rid of local variable duplicating function parameter

--- src/glimp_filter.js
+++ src/glimp_filter.js
@@ -34,73 +34,72 @@
     
     var Filter = function (gl, vertexSource, fragmentSource, callback) {
         // Store WebGL context
-        var _gl = gl;
         var _callback = callback;
         // Create the filter program
-        var _program = _gl.createProgram();
+        var _program = gl.createProgram();
         vertexSource = vertexSource || defaultVertexSource;
         fragmentSource = fragmentSource || defaultFragmentSource;
         fragmentSource = 'precision highp float;' + fragmentSource; // annoying requirement is annoying
-        _gl.attachShader(_program, compileSource(_gl,_gl.VERTEX_SHADER, vertexSource));
-        _gl.attachShader(_program, compileSource(_gl,_gl.FRAGMENT_SHADER, fragmentSource));
-        _gl.linkProgram(_program);
-        if (!_gl.getProgramParameter(_program, _gl.LINK_STATUS)) {
-            throw 'link error: ' + _gl.getProgramInfoLog(_program);
+        gl.attachShader(_program, compileSource(gl,gl.VERTEX_SHADER, vertexSource));
+        gl.attachShader(_program, compileSource(gl,gl.FRAGMENT_SHADER, fragmentSource));
+        gl.linkProgram(_program);
+        if (!gl.getProgramParameter(_program, gl.LINK_STATUS)) {
+            throw 'link error: ' + gl.getProgramInfoLog(_program);
         }
         // Create buffer coordinates
-        var _vertexBuffer = _gl.createBuffer();
-        _gl.bindBuffer(_gl.ARRAY_BUFFER, _vertexBuffer);
-        _gl.bufferData(_gl.ARRAY_BUFFER, new Float32Array([ 0, 0, 0, 1, 1, 0, 1, 1 ]), gl.STATIC_DRAW);
-        var _texCoordBuffer = _gl.createBuffer();
-        _gl.bindBuffer(_gl.ARRAY_BUFFER, _texCoordBuffer);
-        _gl.bufferData(_gl.ARRAY_BUFFER, new Float32Array([ 0, 0, 0, 1, 1, 0, 1, 1 ]), gl.STATIC_DRAW);
+        var _vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, _vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ 0, 0, 0, 1, 1, 0, 1, 1 ]), gl.STATIC_DRAW);
+        var _texCoordBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, _texCoordBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ 0, 0, 0, 1, 1, 0, 1, 1 ]), gl.STATIC_DRAW);
         // Create a framebuffer for this filter
-        var _fb = _gl.createFramebuffer();
+        var _fb = gl.createFramebuffer();
                 
         return {
             apply : function (frameIn,frameOut) {
                 // Bind the framebuffer
                 if(frameOut)
-                    _gl.bindFramebuffer(_gl.FRAMEBUFFER, _fb);
+                    gl.bindFramebuffer(gl.FRAMEBUFFER, _fb);
                 
                 // Set the frameIn texture as input
-                _gl.activeTexture(_gl.TEXTURE0);
-                _gl.bindTexture(_gl.TEXTURE_2D, frameIn.asTexture());
+                gl.activeTexture(gl.TEXTURE0);
+                gl.bindTexture(gl.TEXTURE_2D, frameIn.asTexture());
 
                 // Set coordinates
-                var vertexAttribute = _gl.getAttribLocation(_program, 'vertex');
-                _gl.enableVertexAttribArray(vertexAttribute);
-                var texCoordAttribute = _gl.getAttribLocation(_program, '_texCoord');
-                _gl.enableVertexAttribArray(texCoordAttribute);
-                _gl.useProgram(_program);
-                _gl.bindBuffer(_gl.ARRAY_BUFFER, _vertexBuffer);
-                _gl.vertexAttribPointer(vertexAttribute, 2, gl.FLOAT, false, 0, 0);
-                _gl.bindBuffer(gl.ARRAY_BUFFER, _texCoordBuffer);
-                _gl.vertexAttribPointer(texCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+                var vertexAttribute = gl.getAttribLocation(_program, 'vertex');
+                gl.enableVertexAttribArray(vertexAttribute);
+                var texCoordAttribute = gl.getAttribLocation(_program, '_texCoord');
+                gl.enableVertexAttribArray(texCoordAttribute);
+                gl.useProgram(_program);
+                gl.bindBuffer(gl.ARRAY_BUFFER, _vertexBuffer);
+                gl.vertexAttribPointer(vertexAttribute, 2, gl.FLOAT, false, 0, 0);
+                gl.bindBuffer(gl.ARRAY_BUFFER, _texCoordBuffer);
+                gl.vertexAttribPointer(texCoordAttribute, 2, gl.FLOAT, false, 0, 0);
                 
                 // Use the first texture
-                var texLoc = _gl.getUniformLocation(_program, 'texture');
-                _gl.uniform1i(texLoc, 0);
+                var texLoc = gl.getUniformLocation(_program, 'texture');
+                gl.uniform1i(texLoc, 0);
                 
                 // This callback allows the filter to be specialized
                 if(_callback) {
                     // Get the extra parameters we may have received
                     var args = Array.prototype.slice.call(arguments);
                     // Add gl & program parameters at the beginning
-                    args.splice(0, 0, _gl, _program);
+                    args.splice(0, 0, gl, _program);
                     // Call our callback
                     _callback.apply (this, args);
                 }
 
                 // Set the frameOut texture as output
                 if(frameOut)
-                    _gl.framebufferTexture2D(_gl.FRAMEBUFFER, _gl.COLOR_ATTACHMENT0, _gl.TEXTURE_2D, frameOut.asTexture(), 0);
+                    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, frameOut.asTexture(), 0);
                 
                 // Draw
-                _gl.drawArrays(_gl.TRIANGLE_STRIP, 0, 4);
+                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);                    
                 
                 // Flip results
-                _gl.bindFramebuffer(_gl.FRAMEBUFFER, null);
+                gl.bindFramebuffer(gl.FRAMEBUFFER, null);
             }
         }
     };
