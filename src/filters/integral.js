24bb6105a74a6d5e37bf98eb384544dd545df685
David Corvoysier <david.corvoysier@orange.com>
2013-07-10 16:42:13 +0200

    integral image: Do not pack summed area during the accumulation phases, taking advantage of float textures

--- src/filters/integral.js
+++ src/filters/integral.js
@@ -14,41 +14,28 @@
 (function(global) {
     
     var _initShader,_haddShader,_vaddShader;
+    var _frames;
     
     global.integral = function(frameIn,frameOut,n) {
         n = n || 2;
         var hpass = Math.ceil(Math.log(frameIn.width)/Math.log(n));
         var vpass = Math.ceil(Math.log(frameIn.height)/Math.log(n));
-        var _frames = [global.frame(),global.frame()];
+        var _frames = _frames || [global.frame(),global.frame()];
         var _commonShaderStr = '\
-            float packColor(vec4 color) {\
-                vec4 bitShift = vec4(255.,255.*256.,255.*256.*256.,255.*256.*256.*256.);\
-                return dot(color, bitShift);\
-            }\
-            \
-            vec4 unpackColor(float f) {\
-                vec4 color;\
-                color.a = floor(f / 256.0 / 256.0 / 256.0);\
-                color.b = floor((f - color.a * 256.0 * 256.0 * 256.0) / 256.0 / 256.0);\
-                color.g = floor((f - color.a * 256.0 * 256.0 * 256.0 - color.b * 256.0 * 256.0) / 256.0);\
-                color.r = floor(f - color.a * 256.0 * 256.0 * 256.0 - color.b * 256.0 * 256.0 - color.g * 256.0);\
-                return color / 255.0;\
-            }\
             float offset(float length,float pass) {\
                 return 1.0/length*exp2(pass);\
             }';
         _initShader = _initShader || global.createFilter(
             // Vertex Shader (null uses default)
             null,
-            // Fragment Shader (null uses default)
-            _commonShaderStr +         
+            // Fragment Shader (null uses default)     
             '\
             uniform sampler2D texture;\
             varying vec2 texCoord;\
             void main() {\
                 vec4 color = texture2D(texture, texCoord);\
                 float s = (color.r * .212671 + color.g * .715160 + color.b * .072169)*255.;\
-                gl_FragColor = unpackColor(s);\
+                gl_FragColor = vec4(s,0.,0.,0.);\
             }\
             ',
             // Uniforms callback
@@ -71,8 +58,7 @@
                     gl_FragColor = pixA;\
                 } else {\
                     vec4 pixB = texture2D(texture, texCoord + offset * vec2(-1.,0.));\
-                    float s = packColor(pixA) + packColor(pixB);\
-                    gl_FragColor = unpackColor(s);\
+                    gl_FragColor = pixA + pixB;\
                 }\
             }\
             ',
@@ -101,8 +87,7 @@
                     gl_FragColor = pixA;\
                 } else {\
                     vec4 pixB = texture2D(texture, texCoord + offset * vec2(0.,-1.));\
-                    float s = packColor(pixA) + packColor(pixB);\
-                    gl_FragColor = unpackColor(s);\
+                    gl_FragColor = pixA + pixB;\
                 }\
             }\
             ',
@@ -118,10 +103,10 @@
         for(var i=0;i<hpass;i++){
             _haddShader.run(_frames[i%2],_frames[(i+1)%2],i);
         }
-        for(var i=0;i<vpass-1;i++){
+        for(var i=0;i<vpass;i++){
             _vaddShader.run(_frames[(i+hpass)%2],_frames[(i+hpass+1)%2],i);
         }
-        _vaddShader.run(_frames[(hpass+vpass-1)%2],frameOut,vpass-1);
+        global.pack(_frames[(hpass+vpass)%2],frameOut);
     }
     
 })(glimp);
